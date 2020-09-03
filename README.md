df3c9a8bca32befd111b1af0202057bdc8bc379a
Gustavo Santos <gu.santos91@gmail.com>
2020-09-03 16:57:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tms_Input
+ Captura as informações (números), limpa e adiciona virgula.
---
+++ tms_input.py
@@ -0,0 +1,36 @@
+
+import pyperclip
+import re
+
+
+while True:
+    try:
+        a = int(input("Operacao: "))
+        if a == 1 :
+
+            input_tms = pyperclip.paste()
+            input_tms = input_tms.split("\r\n")
+            input_tms = ",".join(input_tms)# <<< teste
+            input_tms = re.findall('([0-9])', input_tms)
+            print (input_tms)
+            
+    #         print((len(input_tms))-1)
+            
+    #         input_tms = ",".join(input_tms)
+    #         input_tms = input_tms.split("\t")
+    #         input_tms = ",".join(input_tms)
+    #         input_tms = input_tms.split(",,")
+    #         input_tms = ",".join(input_tms)
+    #         input_tms = input_tms.split(",,,")
+    #         input_tms = ",".join(input_tms)
+
+    #         print (input_tms)
+
+    #         pyperclip.copy(input_tms)
+    #     elif a == 0:
+    #         pyperclip.copy(input_tms)
+        else:
+            break
+    except  ValueError:
+        print("Somente numeros para selecao")
+
