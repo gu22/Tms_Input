d3287e98225ef488c88ce5f86c192573dea4e9b7
Gustavo Santos <gu.santos91@gmail.com>
2020-09-11 07:45:10 -0300

    Update tms_input.py
    ajustes

--- tms_input.py
+++ tms_input.py
@@ -12,11 +12,11 @@ while True:
             #input_tms = input_tms.split("\r\n")
            # input_tms = ",".join(input_tms)# <<< teste
             input_tms = re.findall('[0-9]\w+', input_tms)
-            print (input_tms)
+            # print (input_tms)
             
-    #         print((len(input_tms))-1)
+            print((len(input_tms)))
             
-    #         input_tms = ",".join(input_tms)
+            input_tms = ",".join(input_tms)
     #         input_tms = input_tms.split("\t")
     #         input_tms = ",".join(input_tms)
     #         input_tms = input_tms.split(",,")
@@ -24,11 +24,11 @@ while True:
     #         input_tms = input_tms.split(",,,")
     #         input_tms = ",".join(input_tms)
 
-    #         print (input_tms)
+            print (input_tms)
 
-    #         pyperclip.copy(input_tms)
-    #     elif a == 0:
-    #         pyperclip.copy(input_tms)
+            pyperclip.copy(input_tms)
+        elif a == 0:
+            pyperclip.copy(input_tms)
         else:
             break
     except  ValueError:
